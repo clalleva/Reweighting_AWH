401835b69b1cfd2cd4dc306208cac7ebf4e0af75
clalleva <95357440+clalleva@users.noreply.github.com>
2025-04-15 11:47:23 +0200

    Add files via upload

---
+++ arg_Bias.py
@@ -0,0 +1,123 @@
+#########Initializing libraries###############
+
+import time
+
+## To work on data ##
+import numpy as np
+import pandas as pd
+from MDAnalysis.analysis import distances
+import scipy as sc
+
+
+## To plot ##
+from matplotlib import cm 
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+## To work on the trajectory ##
+import MDAnalysis as mda
+   
+## For multiprocessing ##
+import multiprocessing
+from multiprocessing import Pool
+from functools import partial
+
+## Argument parsing ##
+import argparse
+
+#########Parsing the arguments###############
+parser = argparse.ArgumentParser()
+#parser.parse_args()
+
+parser.add_argument("-v","--verbosity", help="increase output verbosity",
+                    action="store_true")
+
+parser.add_argument("--input", help = "Time series of variable of interest", default= "time_EC_IC.npy")
+parser.add_argument("--pullx", help = "Time series of AWH CV", default=  "pullx.xvg")
+parser.add_argument("--awh", help = "Folder in which to find the awh.xvg files", default=  "./")
+parser.add_argument("--folder", help = "Folder in which to save results", default=  "./")
+parser.add_argument("-eb","--extract", help="extract bias",
+                    action="store_true", default=True)
+parser.add_argument("-cw","--calculate", help="calculate weigths from bias",
+                    action="store_true", default=False)
+
+args = parser.parse_args()
+
+if args.verbosity:
+    print("verbosity turned on")
+
+#############################################
+#########Opening files###############
+
+a = np.load(args.input)[::25]#1000]
+if args.verbosity:
+	print (f'Shape of variable input is {a.shape}\nusing first column as time')
+pullx=np.genfromtxt(args.pullx, skip_header=44, skip_footer=2)[::1000,:]#40000,:]
+if args.verbosity:
+        print (f'Shape of pullx input is {pullx.shape}\nusing last column as CV value')
+
+#############################################
+#########Extracting bias###############
+
+def extract_bias(ts_input,awh_input,pullx_input):
+    a = np.load(ts_input)[::25]
+    print (a.shape)
+    pullx=np.genfromtxt(pullx_input, skip_header=44, skip_footer=2)[::1000,:]
+    print (pullx.shape)
+    bias=[]
+    for idx, el in enumerate(a[:,0]):
+        try:
+	    #Open PMF#
+            PMF=np.loadtxt(awh_input+'awh_t%s.xvg' %(int(el)) ,comments=['#','@','&'])
+            all_mins = PMF[:,0]-pullx[idx,-1]
+	    #Find closest value#
+            here=np.where(all_mins**2==np.min(all_mins**2))
+            if args.verbosity==True:
+                print (f'Pullx {pullx[idx,-1]} closest PMF is {PMF[here[0],0]} with energy {PMF[here[0],1]} KJ/mol')
+                print (f'For idx={idx} Pullx value is {pullx[idx,-1]}')
+                print (f'PMF value at {PMF[here[0],0]} is {PMF[here[0],2]}')
+	    #Append closest value#
+            bias.append(PMF[here[0][0],2]) #Using position 2 as bias of awh.xvg
+            
+        except:
+            if args.verbosity==True:
+                print (f'No {el} ps time in awh_results!!!')
+            continue
+    return np.asarray(bias)
+if args.verbosity:
+	print (f'Using position 2 as bias of awh.xvg')
+
+c_bias=extract_bias(args.input,args.awh,args.pullx)
+
+#############################################
+#########Calculating weigths###############
+
+def calculate_weigths(bias, T=303.15):
+	weigths=[]
+
+	R = sc.constants.R
+	if args.verbosity:
+		print (f'Calculating weigths with RT {R*T}, so T {T} K.')
+	for i in bias:
+		w=np.exp((-i/1000)/R*T) # diving i by 1000 to have bigger numbers
+		weigths.append(w)
+		if args.verbosity:
+    			print (f'Energy i')
+	weigths=np.asarray(weigths)
+	return weigths/weigths.sum()
+
+if args.extract:
+	c_weigths=calculate_weigths(c_bias)
+else:
+	c_bias=np.load(args.folder+'calculated_bias.npy')	
+	c_weigths=calculate_weigths(c_bias)
+#############################################
+#########Saving bias###############
+np.save(args.folder+'calculated_bias.npy', np.asarray(c_bias))
+if args.verbosity:
+        print ('Saving file calculated_bias.npy')
+
+#########Saving weigths###############
+np.save(args.folder+'calculated_weigths.npy', np.asarray(c_weigths))
+if args.verbosity:
+        print ('Saving file calculated_weigths.npy')
